839d9aedea2afd98ae5e2029cbfdbe3bce27e574
Shazil Arif <shazilarif94@gmail.com>
2019-10-14 15:50:47 -0400

    more data structure

--- linkedlist.py
+++ linkedlist.py
@@ -1,3 +1,32 @@
-def main():
-    print("Hello world, my name is shazil arif")
-main()
+class LinkedList:
+    def __init__(self, head=None ):
+        self.head = head
+
+    '''
+    @param $position
+        (required) - Assuming the linked list count starts at 0 
+        
+        position is the 'index' of the node to get
+    '''
+    def get_element(self, position):
+        pass
+
+    '''
+    @param $new_element
+        (required) - the node to be appended to the end of the linked list
+        
+        Type: an instance of the Element class found in this package
+    '''
+
+    def append(self, new_element):
+        pass
+
+    '''
+    @param $value
+        (required) -  the value to be deleted from the linked list
+    '''
+    def delete(self, value):
+        pass
+
+    def insert(self):
+        pass
