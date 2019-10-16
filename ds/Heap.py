1e6bc902de4351716900a00ed2be33426ab180b0
Shazil Arif <shazilarif94@gmail.com>
2019-10-16 17:17:22 -0400

    encapsulated bst methods and more heaps

--- ds/Heap.py
+++ ds/Heap.py
@@ -1,9 +1,28 @@
+'''
+A class used to implement the heap data structure using a list
+'''
 class Heap:
     def __init__(self):
         self.items = []
 
+    '''
+    pop removes the first element and returns it
+    '''
     def pop(self):
-        pass
+        if len(self.items)!=0:
+            val = self.items[0]
+            return self.items.pop(0)
+        else:
+            return IndexError
+
+    '''
+    @param
+        type: any
+        (required) - the value to insert into the heap
+    '''
+    def insert(self,value):
+        self.items.append(value)
+        
 
     def heapifyUp(self):
         pass
@@ -13,4 +32,20 @@ class Heap:
     
     def swap(self):
         pass
-    
+
+    def print(self):
+        for i in self.items:
+            print(i)
+
+
+def main():
+    #for testing only    
+    l = Heap()
+    l.insert(5)
+    l.insert(4)
+    l.insert(9)
+    l.insert(22)
+    x = l.pop()
+    l.print()
+main()
+
---
+++ ds/test.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.insert(1,"./ds")
+from .Heap import Heap
+
+l = Heap()
+l.insert(5)
+l.print()
